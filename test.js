54dfd179c024bb0369fa4c10e4aadfa640bdf025
Matt Lloyd <matthew.lloyd@mayden.co.uk>
2016-08-02 18:14:22 +0100

    Compile on the fly tests

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { generate, test as tester } from './index'
+import { generate, test as tester } from './characterise'
 
 test('generates 0 tests', assert => {
   const actual = generate(x => x, x => x)([])
